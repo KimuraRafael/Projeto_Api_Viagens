14407eedfe6d065afec8704c62e8b80aac8229b5
Rafael Kimura <rafaelkimura57@gmail.com>
2024-04-25 19:19:01 -0300

    Recomeço

---
+++ src/main/java/com/ifsp/trabalhoAPI/demo/ApiDeViagensApplication.java
@@ -0,0 +1,13 @@
+package com.ifsp.trabalhoAPI.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDeViagensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDeViagensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifsp/trabalhoAPI/demo/ApiDeViagensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifsp.trabalhoAPI.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDeViagensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
